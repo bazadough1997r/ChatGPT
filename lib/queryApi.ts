db711effabdf6795b90ec0bdf786d60a44870436
Rawan Bazadough <bazadough1997@gmail.com>
2023-03-22 00:15:48 +0300

    firbaseAdmin answering

---
+++ lib/queryApi.ts
@@ -0,0 +1,23 @@
+import openai from "./chatgpt";
+
+const query = async (prompt: string, chatId: string, model: string) => {
+  const res = await openai
+    .createCompletion({
+      model,
+      prompt,
+      temperature: 0.9, // creative answers
+      top_p: 1, // logical answers
+      max_tokens: 1000,
+      frequency_penalty: 0, // idk what exactly its function
+      presence_penalty: 0, // idk what exactly its function
+    })
+    .then((res) => res.data.choices[0].text)
+    .catch(
+      (err) =>
+        `ChatGPT wan unable to find an answer for that! (Error: ${err.message})`
+    );
+
+  return res;
+};
+
+export default query;
