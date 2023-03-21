4eeb1fbaba861979934268dfee7283227a6a8c41
Rawan Bazadough <bazadough1997@gmail.com>
2023-03-22 01:37:26 +0300

    serviceAccount to .env

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,11 +1,11 @@
 import admin from "firebase-admin";
 import { getApps } from "firebase-admin/app";
 
-const serviceAccount = require("./serviceAccountKey.json")
+// const serviceAccount = require("./serviceAccountKey.json")
 
-// const serviceAccount = JSON.parse(
-//   process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
-// );
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
+);
 
 if (!getApps().length) {
   admin.initializeApp({
