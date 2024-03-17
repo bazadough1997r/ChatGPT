37e3faf20bfac646d105c08deb2d2ff45c21afc1
Rawan Bazadough <bazadough1997@gmail.com>
2024-03-17 23:08:03 +0300

    Update chatgpt.ts

--- lib/chatgpt.ts
+++ lib/chatgpt.ts
@@ -1,7 +1,8 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  // apiKey: process.env.OPENAI_API_KEY,
+apiKey: "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",
 });
 
 const openai = new OpenAIApi(configuration);
